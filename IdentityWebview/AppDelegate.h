fafaf384b3900e8a750b37887631a61ab8bc3989
twx <tml@yunhuodemac-mini.local>
2017-03-23 16:21:08 +0800

    Initial Commit

---
+++ IdentityWebview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IdentityWebview
+//
+//  Created by tml on 2017/3/23.
+//  Copyright © 2017年 charles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
